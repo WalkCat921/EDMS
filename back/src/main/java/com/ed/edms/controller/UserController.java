be9a936f93ec5abf8add0fb16b63efb8b6a91aef
Dmitry <20125dimanovogrodsky@gmail.com>
2022-11-18 15:33:25 +0300

    Fixed cross-origin

--- back/src/main/java/com/ed/edms/controller/UserController.java
+++ back/src/main/java/com/ed/edms/controller/UserController.java
@@ -5,6 +5,7 @@ import com.ed.edms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
     @Autowired
     private UserService userService;
--- back/src/main/java/com/ed/edms/modal/Pvi.java
+++
@@ -1,4 +0,0 @@
-package com.ed.edms.modal;
-
-public class Pvi {
-}
